2d00edbed076a34422b593be87c2c9212abb3a8a
Mary Barfield <horizonline109@gmail.com>
2021-05-22 11:25:40 -0500

    Initial commit first comment Work 1 done Work 2 done

--- src/test/java/Hello.java
+++ src/test/java/Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("I m excited to code Java.");
+        System.out.println("Work 1");
+        System.out.println("Wordk 2");
+
     }
 }
